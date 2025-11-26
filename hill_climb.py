0b8be22d54a62f6a4a2587ad9b40fa2c4c7b74c6
Sofia Håbrekke <sofiash@stud.ntnu.no>
2025-11-26 11:39:36 +0100

    Create file for hill climbing algorithm

---
+++ hill_climb.py
@@ -0,0 +1,11 @@
+#  Hill climbing local search algorithm
+
+# Read file 
+
+# Generate a random beginning solution
+
+# Calculate cost of a solution 
+
+# Find neighbors
+
+# Run the hill climbing search algorithm 
